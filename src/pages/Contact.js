b004f1e61fff9928c0cfbb5ba85765522513e363
AdnanTurjoy <aturjoy4@gmail.com>
2022-09-30 01:11:33 +0600

    Normal React Routing

---
+++ src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Contact(props) {
+  return (
+    <div>
+      <h1>Contact Pages</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint tempora,
+        consequuntur harum nulla sed nihil quam maiores sit culpa distinctio ad
+        quos suscipit beatae earum vel, ipsa facere aliquid adipisci recusandae?
+        Enim inventore doloremque quibusdam dicta architecto cumque beatae
+        laborum ex explicabo sapiente, ad sit laudantium, provident accusantium
+        ducimus placeat iusto maiores error eius voluptas harum iste officia?
+        Atque, numquam!
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div>
+      <h1>Home Pages</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio saepe,
+        debitis totam accusamus alias error rem animi, neque iure, nemo pariatur
+        excepturi magni eveniet. Beatae excepturi laboriosam, ullam labore
+        repellat obcaecati voluptatem cum. Laboriosam ipsam ullam sunt
+        voluptates voluptatibus excepturi dolor enim et. A, pariatur incidunt
+        ipsum fugiat ipsa explicabo perspiciatis laudantium error inventore at
+        aspernatur rem amet suscipit earum?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
